f2191ab6b13f621c58b51d4debd9b09503ca37e8
maznievas <maznievas@gmail.com>
2019-04-13 15:08:15 +0300

    fading slick implemented

---
+++ pinecone.js
@@ -0,0 +1,52 @@
+// $('.multiple-slide').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   //количество слайдов, которые выводятся на экране
+//   slidesToScroll: 1
+//   //количество слайдов, которые перелистываются за один раз
+// });
+
+// $('.uncycle').slick({
+//   infinite: false,
+//   speed: 350,
+// // определяем скорость перелистывания
+//   slidesToShow: 4,
+// //количество слайдов для показа
+//   slidesToScroll: 2,
+// //сколько слайдов за раз перелистнется
+// responsive: [
+//     {
+//       breakpoint: 600,
+// //сообщает, при какой ширине экрана нужно включать настройки
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//         infinite: true,
+//       }
+//     }
+// ]
+// });
+
+
+        $('.thing').slick({
+          // dots: true,
+          arrows: false,
+          autoplay: true,
+          infinite: true,
+          loop:true,
+          // autoplaySpeed: 2000,
+            //fade
+            // infinite: true,
+            speed: 400,
+            fade: true,
+            cssEase: 'linear',
+          responsive: [
+            {
+              // critical value for settings
+              breakpoint: 600,
+              settings: {
+              infinite: true,
+            }
+    }
+]
+        });
